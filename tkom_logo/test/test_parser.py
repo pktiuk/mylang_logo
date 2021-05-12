dc1adb31716e1b20c94cff026847e6cc56a98b2a
Paweł Kotiuk <kotiuk@zohomail.eu>
2021-05-12 11:39:54 +0200

    Add checking leaves and nodes in tree

--- tkom_logo/test/test_parser.py
+++ tkom_logo/test/test_parser.py
@@ -8,7 +8,7 @@ from queue import Queue
 module_path = os.path.dirname(os.path.realpath(__file__)) + "/.."
 sys.path.append(module_path)
 
-from shared import Token, TokenType, Location
+from shared import Token, TokenType, Location, ParserNode
 from parser_logo import Parser
 from language_errors import UnexpectedCharacterError, ParseError
 from test_lexer import TextBuffer
@@ -28,30 +28,59 @@ def generate_queue(text) -> Queue:
     return q
 
 
-def check_leaves_and_nodes(tree):
+def check_leaves_and_nodes(tree: ParserNode, print_tree=True):
     """Leaves in tree can be only values,
     middle nodes can be only operators
     """
-    pass
+    LEAF_TOKENS = [TokenType.CONST, TokenType.IDENTIFIER]
+    NODE_TOKENS = [
+        TokenType.ADD_OPERATOR,
+        TokenType.MULT_OPERATOR,
+        TokenType.UNARY_OPERATOR,
+        TokenType.OR_OPERATOR,
+        TokenType.AND_OPERATOR,
+        TokenType.COMP_OPERATOR,
+        TokenType.FIELD_OPERATOR,
+        TokenType.ASSIGNMENT_OPERATOR,
+        TokenType.IF,
+        TokenType.ELSE,
+        TokenType.WHILE,
+        TokenType.FUN,
+    ]
+    NOT_ALLOWED_TOKENS = [TokenType.EOF, TokenType.EOL]
+    if print_tree:
+        print(tree)
+
+    if tree.token.symbol_type in NOT_ALLOWED_TOKENS:
+        raise ValueError("Not allowed token in tree")
+
+    if tree.get_depth() == 0:
+        if tree.token.symbol_type not in LEAF_TOKENS:
+            raise ValueError("Wrong value of leaf token")
+    else:
+        if tree.token.symbol_type not in NODE_TOKENS:
+            raise ValueError("Wrong value of middle node token")
+        for child in tree.children:
+            check_leaves_and_nodes(child, False)
 
 
 def test_parser_stability():
     q = generate_queue("(-33*1)/-2")
     p = Parser(token_source=q)
     result = p.parse()
-    print(result)
+    check_leaves_and_nodes(result)
     q = generate_queue("2+3*4")
     p = Parser(token_source=q)
     result = p.parse()
-    print(result)
+    check_leaves_and_nodes(result)
     q = generate_queue("23==3&& 5>2")
     p = Parser(token_source=q)
     result = p.parse()
-    print(result)
+    check_leaves_and_nodes(result)
     q = generate_queue("2=3&& 5>2")
     p = Parser(token_source=q)
     result = p.parse()
-    print(result)
+    check_leaves_and_nodes(result)
 
 
 def test_multiplications():
@@ -70,6 +99,6 @@ def test_multiplications():
         q.put(token)
     p = Parser(token_source=q)
     result = p.parse()
-    print(result)
+    check_leaves_and_nodes(result)
     assert result.token.value == "*"
     assert result.children[1].token == tokens[-2]
