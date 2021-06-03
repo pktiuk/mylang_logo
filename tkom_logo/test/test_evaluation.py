7cbf2a34e00ffe4ef504a49fac909530d4e34f7a
Paweł Kotiuk <kotiuk@zohomail.eu>
2021-06-03 13:50:00 +0200

    Add test validating glocal and local scopes available in functions

--- tkom_logo/test/test_evaluation.py
+++ tkom_logo/test/test_evaluation.py
@@ -19,6 +19,20 @@ def check_context(context: Context, expected_values: dict):
         assert context.get_element(key) == value
 
 
+def check_execution_with_context_validation(list_of_tuples):
+    for string, values in list_of_tuples:
+        print(f'Generating program: {string}')
+        q = generate_lexer(string)
+        p = Parser(token_source=q)
+        result = p.parse_program()
+        print(result)
+        print("Executing:")
+        result.execute()
+        print("Executed")
+        print(result.root_context)
+        check_context(result.root_context, values)
+
+
 def test_basic_program_with_context():
     TEST_STRINGS = [
         "x=6234", "x=12 y=34 z=x", "x=21+9", "x=43/32 y=3*2", "x=43>32",
@@ -42,17 +56,7 @@ def test_basic_program_with_context():
     }, {
         "x": True
     }]
-    for string, values in zip(TEST_STRINGS, TEST_VALUES):
-        print(f'Generating program: {string}')
-        q = generate_lexer(string)
-        p = Parser(token_source=q)
-        result = p.parse_program()
-        print(result)
-        print("Executing:")
-        result.execute()
-        print("Executed")
-        print(result.root_context)
-        check_context(result.root_context, values)
+    check_execution_with_context_validation(zip(TEST_STRINGS, TEST_VALUES))
 
 
 def test_block_constructions():
@@ -65,19 +69,28 @@ def test_block_constructions():
         }""", {
         "x": 1,
         "y": 1
+    }), ("x=0 if(23+3>1){x=1} else{x=2}", {
+        "x": 1
     })]
 
-    for string, values in TEST_TUPLES:
-        print(f'Generating program: {string}')
-        q = generate_lexer(string)
-        p = Parser(token_source=q)
-        result = p.parse_program()
-        print(result)
-        print("Executing:")
-        result.execute()
-        print("Executed")
-        print(result.root_context)
-        check_context(result.root_context, values)
+    check_execution_with_context_validation(TEST_TUPLES)
+
+
+def test_functions():
+    TEST_TUPLES = [("""glob=0
+    x=-3
+    fun foo(x)
+    {
+        x=x+3"""
+                    """
+        glob=x
+    }
+    foo(11)
+    """, {
+                        "glob": 11 + 3,
+                        "x": -3
+                    })]
+    check_execution_with_context_validation(TEST_TUPLES)
 
 
 def test_context():
