9bb18c4fb5c837e2d86f16a4915dab4eb445e4d9
Paweł Kotiuk <kotiuk@zohomail.eu>
2021-07-14 17:45:03 +0200

    Allow execution and printing errors

--- mylang_rest_server.py
+++ mylang_rest_server.py
@@ -2,12 +2,16 @@
 
 from flask import Flask, request
 
+from mylang.shared import StringLogger, set_global_logger, get_global_logger
+
 from mylang.language_errors import LogoSyntaxError, BaseLanguageException
 from mylang.lexer import Lexer
 from mylang.parser_logo import Parser
 from mylang.text_reader import StringReader
 
 app = Flask(__name__)
+
+set_global_logger(StringLogger())
 
 
 @app.route('/', methods=["GET"])
@@ -27,7 +31,7 @@ def post_code():
 
     log, canvas, error = execute_code(code)
     response = {}
-    response["log"] = "hello"
+    response["log"] = log
     response["canvas"] = None
     response["error"] = error
     return response
@@ -35,18 +39,18 @@ def post_code():
 
 def execute_code(code: str):
     reader = StringReader(code)
-    lexer = Lexer(reader)
+    lexer = Lexer(reader, get_global_logger())
     try:
-        program = Parser(lexer).parse_program()
+        program = Parser(lexer, get_global_logger()).parse_program()
         program.execute()
-    except LogoSyntaxError as err:
-        return ("", None, err.str())
     except BaseLanguageException as exc:
-        error_msg = f"Error: {exc.args[0]}"
-        error_msg += f"At: {exc.location}"
+        error_msg = f"Error: {str(exc)}\n"
+        error_msg += f"At: {exc.location}\n"
         error_msg += reader.get_loc_region(exc.location)
         return ("", None, error_msg)
-    return ("finished", None, "")  #TODO
+
+    canvas = program.get_canvas()
+    return (get_global_logger().out_string, None, None)  #TODO draw canvas
 
 
 if __name__ == '__main__':
